6f4ccf4af3a3fe3fbe8e9a9024e85179775f0051
Carlos Ché <3013216470101@ingenieria.usac.edu.gt>
2023-03-10 02:04:13 -0600

    Facebook Post Code
    Facebook Post Code Implementation and test with code

--- FacebookPost/Program.cs
+++ FacebookPost/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<string> namesList = new List<string>();
+            namesList = AskNames();
+            DisplayMessage(namesList);
+        }
+
+        static List<string> AskNames()
+        {
+            List<string> namesList = new List<string>();
+            string name;
+            
+            do
+            {
+                Console.Write("Type a name or press enter to finish: ");
+                name = Console.ReadLine();
+                namesList.Add(name);
+            } while ( !String.IsNullOrWhiteSpace(name));
+
+            return namesList;
+        }
+
+        static void DisplayMessage(List<string> namesList)
+        {
+            int namesListSize = namesList.Count - 1 ;
+
+            switch (namesListSize)
+            {   
+                case 0:
+                    {
+                        Console.WriteLine("");
+                        break;
+                    }
+                case 1:
+                    {
+                        string name = namesList[0];
+                        Console.WriteLine("{0} likes your post", name);
+                        break;
+                    }
+                case 2:
+                    {
+                        string nameOne = namesList[0];
+                        string nameTwo = namesList[1];
+                        Console.WriteLine("{0} and {1} like your post", nameOne, nameTwo);
+                        break;
+                    }
+                default:
+                    {
+                        string nameOne = namesList[0];
+                        string nameTwo = namesList[1];
+                        Console.WriteLine("{0}, {1} and {2} others like your post", nameOne, nameTwo, namesListSize - 2);
+                        break;
+                    }
+            }
         }
     }
 }
